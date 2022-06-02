f49b2cf7e1146595bbb9554ca26dba2828898df0
samuel <samuel123@gmail.com>
2022-06-02 11:00:14 +0800

    master更改

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("new file");
         System.out.println("第一次更新");
+        System.out.println("master更改");
     }
 }
